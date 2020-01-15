2dea318858024af011b3220754ee8d9f596372e0
bartlomiej.duda <ikskoks@gmail.com>
2020-01-15 17:11:28 +0100

    Tasks01 15.01.2929 17:11

---
+++ SimplePythonTasks/Tasks01.py
@@ -0,0 +1,42 @@
+import sys
+import datetime
+from math import pi
+
+# Exercises - https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#print(sys.version)
+#now = datetime.datetime.now()
+#print("Cur date: " + now.strftime("%Y-%m-%d %H:%M:%S"))
+#r = float(input("Input r: "))
+#print("pi*r^2 = " + str(pi * r**2))
+
+#zad5
+#name = input("Name: ")
+#surname = input("Surname: ")
+#print(surname + " " + name)
+
+#zad6
+#vals = input("Vals: ")
+#list1 = vals.split(',')
+#tuple1 = tuple(list1)
+#print(list1)
+#print(tuple1)
+
+#zad7
+#filename1 = input("Filename: ")
+#ext1 = filename1.split('.')[-1]
+#print('Ext: ' + str(ext1))
+
+#zad8
+#color_list = ["Red","Green","White" ,"Black"]
+#print(color_list[0] + " " + color_list[-1])
+
+#zad9
+#exam_st_date = (11, 12, 2014)
+#print("St date: %i/%i/%i"%exam_st_date)
+
+#zad10
+#n = int(input("n: "))
+#w = n + (n*n) + (n*n*n) 
+#print("w: " + str(w))
+
+#zad11
